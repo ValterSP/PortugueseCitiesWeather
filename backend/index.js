8cdad7e2ba156e8a8235e6fc07555d7cc3097e29
Valter Pedrosa <2210991@my.ipleiria.pt>
2024-08-19 22:02:45 +0100

    Create a map with the city name and respective number code for openweather

--- backend/index.js
+++ backend/index.js
@@ -2,8 +2,20 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+const citiesMap = new Map();
+
+citiesMap.set('leiria', 2267094);
+citiesMap.set('lisbon', 2267056);
+citiesMap.set('porto', 2735941);
+citiesMap.set('faro', 2268337);
+citiesMap.set('coimbra', 2740636);
+
 app.get('/api/weather/:city', (req, res) => {
     const city = req.params.city;
+    if (!citiesMap.has(city)) {
+        res.status(404).send(`City ${city} not found`);
+        return;
+    }
     res.send(`City is ${city}`);
 });
 
