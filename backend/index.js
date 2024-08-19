2682102810f00e8b21665291d74e770ceba9d82a
Valter Pedrosa <2210991@my.ipleiria.pt>
2024-08-19 22:32:04 +0100

    Add a cache of 30 minutes

--- backend/index.js
+++ backend/index.js
@@ -6,12 +6,16 @@ const port = 3000;
 
 const citiesMap = new Map();
 const apiKey = process.env.API_KEY;
+const weatherCache = new Map();
 
 citiesMap.set('leiria', 2267094);
-citiesMap.set('lisbon', 2267056);
+citiesMap.set('lisboa', 2267056);
 citiesMap.set('porto', 2735941);
 citiesMap.set('faro', 2268337);
 citiesMap.set('coimbra', 2740636);
+
+const minutes = 30;
+const milliseconds = minutes * 60 * 1000;
 
 app.get('/api/weather/:city', (req, res) => {
     const city = req.params.city.toLowerCase();
@@ -20,9 +24,17 @@ app.get('/api/weather/:city', (req, res) => {
         return;
     }
     const cityId = citiesMap.get(city);
+    if (weatherCache.has(cityId)) {
+        const cachedWeather = weatherCache.get(cityId);
+        if (cachedWeather.timestamp + milliseconds > Date.now()) {
+            res.send(cachedWeather.data.list);
+            return;
+        }
+    }
     axios.get(`http://api.openweathermap.org/data/2.5/forecast?id=${cityId}&appid=${apiKey}`)
         .then(response => {
-            res.send(response.data);
+            weatherCache.set(cityId, {timestamp: Date.now(), data: response.data});
+            res.send(response.data.list);
         })
         .catch(error => {
             res.status(500).send(error);
