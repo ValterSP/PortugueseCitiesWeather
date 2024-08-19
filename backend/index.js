a14f385edb9192e785bf08459d89a7a09345182e
Valter Pedrosa <2210991@my.ipleiria.pt>
2024-08-19 22:15:29 +0100

    API call to openweathermap

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,11 @@
 const express = require('express');
+require('dotenv').config();
+const axios = require('axios');
 const app = express();
 const port = 3000;
 
 const citiesMap = new Map();
+const apiKey = process.env.API_KEY;
 
 citiesMap.set('leiria', 2267094);
 citiesMap.set('lisbon', 2267056);
@@ -11,12 +14,19 @@ citiesMap.set('faro', 2268337);
 citiesMap.set('coimbra', 2740636);
 
 app.get('/api/weather/:city', (req, res) => {
-    const city = req.params.city;
+    const city = req.params.city.toLowerCase();
     if (!citiesMap.has(city)) {
         res.status(404).send(`City ${city} not found`);
         return;
     }
-    res.send(`City is ${city}`);
+    const cityId = citiesMap.get(city);
+    axios.get(`http://api.openweathermap.org/data/2.5/forecast?id=${cityId}&appid=${apiKey}`)
+        .then(response => {
+            res.send(response.data);
+        })
+        .catch(error => {
+            res.status(500).send(error);
+        });
 });
 
 app.listen(port, () => {
